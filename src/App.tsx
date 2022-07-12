f31f333bdc35f451cbe9a309994d26d478a32fc9
alpetrushkin <alpetrushkin1984@mail.ru>
2022-07-12 21:59:11 +0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import './App.css';
 
+function hello() {
+    debugger
+    alert('Hello IT-Kamasutra')
+}
+// hello()
+
 function App() {
+    debugger
   return (
-    <div className="App">
-     Hello, samurai!
+    <div>
+        This is APP Component
+        <Rating/>
+        <Accordion/>
     </div>
   );
+}
+
+function Rating() {
+    debugger
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Star() {
+    return <div>star</div>
+}
+
+function Accordion() {
+    debugger
+    return <div>
+        <h3>Меню</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
 }
 
 export default App;
