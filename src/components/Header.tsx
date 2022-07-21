058aec378f9850e0c26f25cd5b1f5adc890e42da
alpetrushkin <alpetrushkin1984@mail.ru>
2022-07-21 20:00:46 +0300

    css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css'
 
 export const Header = () => {
    return (
-      <header>
+      <header className={s.header}>
          <img src="https://cdn-icons-png.flaticon.com/512/3800/3800024.png" alt=""/>
       </header>
    );
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 export const Profile = () => {
    return (
-      <div className='content'>
+      <div className={s.content}>
          <div>
             <img src="https://klike.net/uploads/posts/2019-11/1572608842_3.jpg" alt=""/>
          </div>
@@ -14,11 +15,11 @@ export const Profile = () => {
             <div>
                New post
             </div>
-            <div>
-               <div>
+            <div className={s.posts}>
+               <div className={s.item}>
                   post 1
                </div>
-               <div>
+               <div className={s.item}>
                   post 2
                </div>
             </div>
