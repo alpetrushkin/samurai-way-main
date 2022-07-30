ee164e3eecaa5cf6ae3bb446f526187fa453b629
alpetrushkin <alpetrushkin1984@mail.ru>
2022-07-30 05:28:45 +0300

    props для Диалогов

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,33 +2,47 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+type DialogItemType = {
+   name: string
+   id: string
+}
+
+const DialogItem = (props: DialogItemType) => {
+   let path = '/dialogs/' + props.id
+
+   return (
+      <div className={s.dialog + ' ' + s.active}>
+         <NavLink to={path}>{props.name}</NavLink>
+      </div>
+   )
+}
+
+type MessageType = {
+   message: string
+}
+
+const Message = (props: MessageType) => {
+   return (
+      <div className={s.dialog}>{props.message}</div>
+   )
+}
+
 export const Dialogs = () => {
    return (
       <div className={s.dialogs}>
          <div className={s.dialogItems}>
-            <div className={s.dialog + ' ' + s.active}>
-               <NavLink to='/dialogs/1'>Alex</NavLink>
-            </div>
-            <div className={s.dialog}>
-               <NavLink to='/dialogs/2'>Denis</NavLink>
-            </div>
-            <div className={s.dialog}>
-               <NavLink to='/dialogs/3'>Mark</NavLink>
-            </div>
-            <div className={s.dialog}>
-               <NavLink to='/dialogs/4'>Olga</NavLink>
-            </div>
-            <div className={s.dialog}>
-               <NavLink to='/dialogs/5'>Dimych</NavLink>
-            </div>
-            <div className={s.dialog}>
-               <NavLink to='/dialogs/6'>Valera</NavLink>
-            </div>
+            <DialogItem name='Alex' id='1'/>
+            <DialogItem name='Denis' id='2'/>
+            <DialogItem name='Mark' id='3'/>
+            <DialogItem name='Olga' id='4'/>
+            <DialogItem name='Dimych' id='5'/>
+            <DialogItem name='Valera' id='6'/>
          </div>
          <div className={s.messages}>
-            <div className={s.message}>Hi</div>
-            <div className={s.message}>How is your it-kamasutra</div>
-            <div className={s.message}>Yo</div>
+            <Message message='Hi' />
+            <Message message='How is your it-kamasutra' />
+            <Message message='Yo' />
+            <Message message='Hello' />
          </div>
       </div>
 
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import s from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
 export const Profile = () => {
    return (
       <div className={s.content}>
-         <div>
-            <img src="https://klike.net/uploads/posts/2019-11/1572608842_3.jpg" alt=""/>
-         </div>
-         <div>
-            ava + description
-         </div>
+         <ProfileInfo />
          <MyPosts />
       </div>
    );
